16616cbcac9ec088cb0837dbabe772a70d02a8d2
Gherbi Hicham <gherbixhicham@gmail.com>
2016-06-11 00:43:40 +0100

    deleting the content of a file

--- src/model.java
+++ src/model.java
@@ -6,6 +6,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.sql.Connection;
 import java.sql.Date;
@@ -321,7 +322,11 @@ public void writdayabasepath(String dbpath, File f){
 	    }	
 		
 }
-
+//this empties the file that contains the db path
+public void emptyFile (File f) throws FileNotFoundException{
 	
+	PrintWriter pw = new PrintWriter(f);
+	pw.close();
+}
 	
 }
