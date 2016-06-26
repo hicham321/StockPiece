1b51a6593a15627a3dbe2e473d2cc25c357a042c
Gherbi Hicham <gherbixhicham@gmail.com>
2016-06-26 23:35:04 +0100

    setting title to addingquantity and code indentation

--- src/addingquantity.java
+++ src/addingquantity.java
@@ -13,178 +13,180 @@ import javax.swing.UIManager;
 import org.jdesktop.swingx.autocomplete.AutoCompleteDecorator;
 
 public class addingquantity extends JFrame{
-	
-		    //for the name of the product 
-		    private JButton ajoutlot = new JButton("ajout");
-			
-			private JButton modifieLot = new JButton("modifie");
-			
-			private JButton supprimLot = new JButton("supprimé");
-			
-			final DefaultComboBoxModel ajoutprodnom = new DefaultComboBoxModel();
-			
-			final JComboBox ajoutproduit = new JComboBox(ajoutprodnom); 
-			
-		    //	final JComboBox ajoutref = new JComboBox();    
 
-			
-		    private JScrollPane ProduitNomListScrollPane = new JScrollPane(ajoutproduit); 
-		    			
-			private JTextField qte = new JTextField(10);
-			
-		    private JLabel labqte = new JLabel("الكمية");
-			
-			private JTextField motpasstext = new JTextField(10);
-			
-			private JButton annule = new JButton("الغاء");
-			
-			private JButton ok = new JButton("موافقة");
-			
-			//lower end of jpanel will contain labels for Lot quantity, global quantity and price
-		    private JLabel labQteLot = new JLabel("الكمية الجزئية");
-		    private JLabel labQteGlobal = new JLabel("الكمية الكلية");
-		    private JLabel labprixAchat = new JLabel("ثمن الشراء");
-		    private JLabel labprixVente = new JLabel("ثمن البيع");
-		    
-		    //these are modified labels to be set to the buy and sell prices and qte Lot and qte globale
-		    private JLabel prixAchat = new JLabel(" ");
-		    private JLabel prixVente = new JLabel(" ");
-		    private JLabel qteLot  = new JLabel(" ");
-		    private JLabel qteGlobal = new JLabel(" ");
+	//for the name of the product 
+	private JButton ajoutlot = new JButton("ajout");
 
-			//for the reference of the product
-			//the model's only job is to hold information
-            final DefaultComboBoxModel ajoutprodmodel = new DefaultComboBoxModel();
-            
-            
-        	final JComboBox ajoutProduitComboBox  = new JComboBox(ajoutprodmodel);    
-						
-		    private JScrollPane ajoutProduitListScrol = new JScrollPane(ajoutProduitComboBox); 
-		    
-		    //this is for the product type
-            final DefaultComboBoxModel ajoutRefmodel = new DefaultComboBoxModel();
-            
-        	final JComboBox ajoutRefComboBox  = new JComboBox(ajoutRefmodel);    
-						
-		    private JScrollPane ajoutRefListScrol = new JScrollPane(ajoutRefComboBox); 
+	private JButton modifieLot = new JButton("modifie");
 
-			
-		public addingquantity(){
-				
-				setResizable(true);
-		        setSize(400,400);        
-		        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		        setLocationRelativeTo(null);
-		        
-		        this.ajoutprodmodel.addElement("dcd");
-		        this.ajoutprodmodel.addElement("fgfg");
-		        this.ajoutprodmodel.addElement("dfcvbbcd");
-		        this.ajoutprodmodel.addElement("qsdqsd");
-		        this.ajoutprodmodel.addElement("klmkjlm");
-		        this.ajoutprodmodel.addElement("uioio");
-		        this.ajoutprodmodel.addElement("azee");
-		        this.ajoutprodmodel.addElement("xwxcxw");
-                
-		        AutoCompleteDecorator.decorate(ajoutProduitComboBox);
-		        ajoutProduitComboBox.setSelectedIndex(0);
-		        
-		        this.ajoutRefmodel.addElement("34");
-		        this.ajoutRefmodel.addElement("84784");
-		        this.ajoutRefmodel.addElement("8747");
-		        this.ajoutRefmodel.addElement("12132");
-		        this.ajoutRefmodel.addElement("1200998");
-		        this.ajoutRefmodel.addElement("3626");
-		        this.ajoutRefmodel.addElement("5343");
-		        this.ajoutRefmodel.addElement("2346544");
-		        
-		        AutoCompleteDecorator.decorate(ajoutRefComboBox);
-		        ajoutRefComboBox.setSelectedIndex(0);
-		        
-		        
-		        JPanel panel = new JPanel();
-		        
-		        panel.add(qte);
-		        panel.add(labqte);
-		        panel.add(labQteLot);
-		        panel.add(labQteGlobal);
-		        panel.add(labprixAchat);
-		        panel.add(labprixVente);
-		        panel.add(prixAchat);
-		        panel.add(prixVente);
-		        panel.add(qteGlobal);
-		        panel.add(qteLot);
+	private JButton supprimLot = new JButton("supprimé");
+
+	final DefaultComboBoxModel ajoutprodnom = new DefaultComboBoxModel();
+
+	final JComboBox ajoutproduit = new JComboBox(ajoutprodnom); 
+
+	//	final JComboBox ajoutref = new JComboBox();    
 
 
-		        panel.add(ok);
-		        panel.add(annule);
-		        panel.add(ajoutProduitComboBox);
-		        panel.add(ajoutRefComboBox);
-		        this.add(panel);
-		        
-				
-			}
-		
-		public void addajoutlistner(ActionListener listner){
-			
-		
-		}
-		
-		
-		   
-			public JButton getAjoutlot() {
-				return ajoutlot;
-			}
-			public void setAjoutlot(JButton ajoutlot) {
-				this.ajoutlot = ajoutlot;
-			}
-			public JButton getModifieLot() {
-				return modifieLot;
-			}
-			public void setModifieLot(JButton modifieLot) {
-				this.modifieLot = modifieLot;
-			}
-			public JScrollPane getFruitListScrollPane() {
-				return ProduitNomListScrollPane;
-			}
-			public void setFruitListScrollPane(JScrollPane fruitListScrollPane) {
-				this.ProduitNomListScrollPane = fruitListScrollPane;
-			}
-			public DefaultComboBoxModel getFruitsName() {
-				return ajoutprodnom;
-			}
-			public JComboBox getAjoutproduit() {
-				return ajoutproduit;
-			}
+	private JScrollPane ProduitNomListScrollPane = new JScrollPane(ajoutproduit); 
 
-			public JButton getAnnule() {
-				return annule;
-			}
+	private JTextField qte = new JTextField(10);
 
-			public JButton getOk() {
-				return ok;
-			}
+	private JLabel labqte = new JLabel("الكمية");
 
-			public void setPrixAchat(JLabel prixAchat) {
-				this.prixAchat = prixAchat;
-			}
+	private JTextField motpasstext = new JTextField(10);
 
-			public void setPrixVente(JLabel prixVente) {
-				this.prixVente = prixVente;
-			}
+	private JButton annule = new JButton("الغاء");
 
-			public void setQteLot(JLabel qteLot) {
-				this.qteLot = qteLot;
-			}
+	private JButton ok = new JButton("موافقة");
 
-			public void setQteGlobal(JLabel qteGlobal) {
-				this.qteGlobal = qteGlobal;
-			}
-            
-			
-		    
-		    
-		    
+	//lower end of jpanel will contain labels for Lot quantity, global quantity and price
+	private JLabel labQteLot = new JLabel("الكمية الجزئية");
+	private JLabel labQteGlobal = new JLabel("الكمية الكلية");
+	private JLabel labprixAchat = new JLabel("ثمن الشراء");
+	private JLabel labprixVente = new JLabel("ثمن البيع");
+
+	//these are modified labels to be set to the buy and sell prices and qte Lot and qte globale
+	private JLabel prixAchat = new JLabel(" ");
+	private JLabel prixVente = new JLabel(" ");
+	private JLabel qteLot  = new JLabel(" ");
+	private JLabel qteGlobal = new JLabel(" ");
+
+	//for the reference of the product
+	//the model's only job is to hold information
+	final DefaultComboBoxModel ajoutprodmodel = new DefaultComboBoxModel();
+
+
+	final JComboBox ajoutProduitComboBox  = new JComboBox(ajoutprodmodel);    
+
+	private JScrollPane ajoutProduitListScrol = new JScrollPane(ajoutProduitComboBox); 
+
+	//this is for the product type
+	final DefaultComboBoxModel ajoutRefmodel = new DefaultComboBoxModel();
+
+	final JComboBox ajoutRefComboBox  = new JComboBox(ajoutRefmodel);    
+
+	private JScrollPane ajoutRefListScrol = new JScrollPane(ajoutRefComboBox); 
+
+
+	public addingquantity(){
+
+		setResizable(true);
+		setTitle("ادخال منتوج");
+		setSize(400,400);        
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		//setLayout(null);
+
+		this.ajoutprodmodel.addElement("dcd");
+		this.ajoutprodmodel.addElement("fgfg");
+		this.ajoutprodmodel.addElement("dfcvbbcd");
+		this.ajoutprodmodel.addElement("qsdqsd");
+		this.ajoutprodmodel.addElement("klmkjlm");
+		this.ajoutprodmodel.addElement("uioio");
+		this.ajoutprodmodel.addElement("azee");
+		this.ajoutprodmodel.addElement("xwxcxw");
+
+		AutoCompleteDecorator.decorate(ajoutProduitComboBox);
+		ajoutProduitComboBox.setSelectedIndex(0);
+
+		this.ajoutRefmodel.addElement("34");
+		this.ajoutRefmodel.addElement("84784");
+		this.ajoutRefmodel.addElement("8747");
+		this.ajoutRefmodel.addElement("12132");
+		this.ajoutRefmodel.addElement("1200998");
+		this.ajoutRefmodel.addElement("3626");
+		this.ajoutRefmodel.addElement("5343");
+		this.ajoutRefmodel.addElement("2346544");
+
+		AutoCompleteDecorator.decorate(ajoutRefComboBox);
+		ajoutRefComboBox.setSelectedIndex(0);
+
+
+		JPanel panel = new JPanel();
+
+		panel.add(qte);
+		panel.add(labqte);
+		panel.add(labQteLot);
+		panel.add(labQteGlobal);
+		panel.add(labprixAchat);
+		panel.add(labprixVente);
+		panel.add(prixAchat);
+		panel.add(prixVente);
+		panel.add(qteGlobal);
+		panel.add(qteLot);
+
+
+		panel.add(ok);
+		panel.add(annule);
+		panel.add(ajoutProduitComboBox);
+		panel.add(ajoutRefComboBox);
+		this.add(panel);
+
+
+	}
+
+	public void addajoutlistner(ActionListener listner){
+
+
+	}
+
+
+
+	public JButton getAjoutlot() {
+		return ajoutlot;
+	}
+	public void setAjoutlot(JButton ajoutlot) {
+		this.ajoutlot = ajoutlot;
+	}
+	public JButton getModifieLot() {
+		return modifieLot;
+	}
+	public void setModifieLot(JButton modifieLot) {
+		this.modifieLot = modifieLot;
+	}
+	public JScrollPane getFruitListScrollPane() {
+		return ProduitNomListScrollPane;
+	}
+	public void setFruitListScrollPane(JScrollPane fruitListScrollPane) {
+		this.ProduitNomListScrollPane = fruitListScrollPane;
+	}
+	public DefaultComboBoxModel getFruitsName() {
+		return ajoutprodnom;
+	}
+	public JComboBox getAjoutproduit() {
+		return ajoutproduit;
+	}
+
+	public JButton getAnnule() {
+		return annule;
+	}
+
+	public JButton getOk() {
+		return ok;
+	}
+
+	public void setPrixAchat(JLabel prixAchat) {
+		this.prixAchat = prixAchat;
+	}
+
+	public void setPrixVente(JLabel prixVente) {
+		this.prixVente = prixVente;
+	}
+
+	public void setQteLot(JLabel qteLot) {
+		this.qteLot = qteLot;
+	}
+
+	public void setQteGlobal(JLabel qteGlobal) {
+		this.qteGlobal = qteGlobal;
+	}
+
+
+
+
+
 }
-		    
-			
+
+
 			
