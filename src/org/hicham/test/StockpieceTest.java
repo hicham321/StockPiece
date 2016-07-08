f65da3ced7bf4293e375eb53e2487906b566f496
Gherbi Hicham <gherbixhicham@gmail.com>
2016-07-08 16:16:02 +0100

    adding Junit test cases to the project

---
+++ src/org/hicham/test/StockpieceTest.java
@@ -0,0 +1,14 @@
+package org.hicham.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StockpieceTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
