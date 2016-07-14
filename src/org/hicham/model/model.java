94698617a9cfdbf1cea78e40d86ffcf44c0f1d21
Gherbi Hicham <gherbixhicham@gmail.com>
2016-07-14 14:46:02 +0100

    adding isInteger method to te model class

--- src/org/hicham/model/model.java
+++ src/org/hicham/model/model.java
@@ -352,6 +352,31 @@ to a stored salted hash of the password. */
 				);
 		return Base64.encodeBase64String(key.getEncoded());
 	}
+	//is Integer method **checks if a given String is an Integer** 
+	public static boolean isInteger(String str) {
+	    if (str == null) {
+	        return false;
+	    }
+	    int length = str.length();
+	    if (length == 0) {
+	        return false;
+	    }
+	    int i = 0;
+	    if (str.charAt(0) == '-') {
+	        if (length == 1) {
+	            return false;
+	        }
+	        i = 1;
+	    }
+	    for (; i < length; i++) {
+	        char c = str.charAt(i);
+	        if (c < '0' || c > '9') {
+	            return false;
+	        }
+	    }
+	    return true;
+	}
+
 	
 	//generation a random number 
 	public int generateRandomNumber(){
