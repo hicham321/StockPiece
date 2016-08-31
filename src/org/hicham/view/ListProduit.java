9d4f864c507fe9b0b77bac98295f46919668b237
Gherbi Hicham <gherbixhicham@gmail.com>
2016-08-31 23:39:30 +0100

    trying to connect the table model from controller to the view

--- src/org/hicham/view/ListProduit.java
+++ src/org/hicham/view/ListProduit.java
@@ -5,6 +5,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Vector;
 
 import javax.swing.GroupLayout;
 import javax.swing.JButton;
@@ -19,32 +20,53 @@ import org.hicham.model.model;
 
 public class ListProduit extends JPanel{
 	
-	JTable ListProduitTable;
 	
-	JScrollPane scrolPane;
-    JButton buttt = new JButton("dsdfdf");
+	private DefaultTableModel dt= new DefaultTableModel();
+	
+	private JTable ListProduitTable=new JTable(dt);
+	
+	private JScrollPane scrolPane;
+	
+    private JButton buttt = new JButton("dsdfdf");
     
 	public ListProduit(){
-		try{
-		model m = new model();
-		ResultSet rs= m.listProduitToutResultat();
-        DefaultTableModel dtm = model.buildTableModel(rs);
+		//try{
+		
+
+//	    Vector rowData = new Vector<String>();
+//	    rowData.addElement("sdds");
+//	    dtm.addRow(rowData);
+
+        //this.ListProduitTable = new JTable(dtm);
         
-        this.ListProduitTable = new JTable(dtm);
 		this.setLayout(new GroupLayout(this));
 		this.setBackground(Color.decode("#CFDBC5"));
 		this.add(buttt);
 		ListProduitTable.setPreferredScrollableViewportSize(new Dimension(500,50));
 		ListProduitTable.setFillsViewportHeight(true);
+		ListProduitTable.setFillsViewportHeight( true );
 		scrolPane= new JScrollPane(ListProduitTable);
 		scrolPane.setBounds(100, 100, 500, 500);
         this.add(scrolPane);	
 		this.setVisible(true);
-		}catch(SQLException ex){
-			ex.printStackTrace();
-		}
+//		}catch(SQLException ex){
+//			ex.printStackTrace();
+//		}
 
 	} 
+//This here should be the setter for the table model to relate this view dynamically to the	model class
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	//to keep the Jtable separate from the model you can call the Jtable from the controller once you click the view 
 	/*this is how to sort a Jtable (without needing to create other queries)
